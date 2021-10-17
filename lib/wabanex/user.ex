f5264de0b33c0ae1c7491ccee5c123756efe69f4
Rafael Marinho <rafael_mf3@hotmail.com>
2021-10-17 15:31:44 -0300

    users contexts create and get

--- lib/wabanex/user.ex
+++ lib/wabanex/user.ex
@@ -2,6 +2,8 @@ defmodule Wabanex.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+
   @fields [:email, :password, :name]
 
   schema "users" do
